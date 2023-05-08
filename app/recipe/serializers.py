3a0747e82fdf988e5a68e2067fb4168f498e3283
Rev Tan <tanyunzhe98@gmail.com>
2023-05-08 11:11:20 -0400

    add more tests.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,3 +12,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Metal(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
